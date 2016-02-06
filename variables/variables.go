caff517698705cd3abaec24a15d4459739607d3c
Sebastian Duque <sebas_tian_95@hotmail.com>
2016-02-05 23:41:42 -0500

    variables with GO

---
+++ variables/variables.go
@@ -0,0 +1,17 @@
+package main;
+
+import "fmt";
+
+func main() {
+  // var nombre_variable tipo_dato
+  /* Examples
+  var x, y, z int;
+  var cadena string;
+  var bandera bool;
+  var cadenas []string;*/
+
+  // Inicializacion de variables con :=
+  nombre := "Coco";
+  nombre = "cocooooo";
+  fmt.Println(nombre);
+}
